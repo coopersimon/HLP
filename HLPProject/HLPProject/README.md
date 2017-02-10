e5dff459530628094047942c146c978c1e0b4abf
Simon <simon.d.cooper@hotmail.co.uk>
2017-02-10 21:46:41 +0000

    Added basic Tokeniser framework, along with basic Test framework

---
+++ HLPProject/HLPProject/README.md
@@ -0,0 +1,16 @@
+# HLP !!!
+
+## Parser!
+String -> Instruction Array
+
+## Interpreter
+State -> Instruction Array -> State
+
+## Using the test framework
+Create a separate file for your individual tests! Each source file should have its own test file, in namespace Test.
+
+Simple tests consist of a (function * expectedValue) List. The framework then compares these with compareList.
+
+## TODO:
+
+[x] Tokeniser base
---
+++ HLPProject/HLPProject/TokeniserTest.fs
@@ -0,0 +1,22 @@
+﻿// Test code for the tokeniser.
+
+namespace Test
+module Tokeniser =
+    
+    open Parse.Tokeniser
+
+    (*let test_stringToToken =
+        printfn "%A" (stringToToken "MOV")
+        printfn "%A" (stringToToken "R3")
+        printfn "%A" (stringToToken "R10")
+        printfn "%A" (stringToToken "R13")
+        printfn "%A" (stringToToken "R25")
+        printfn "%A" (stringToToken "mov")*)
+
+    let test_stringToToken =
+        [(stringToToken "MOV", T_MOV);
+        (stringToToken "R3", T_REG 3);
+        (stringToToken "R10", T_REG 10);
+        (stringToToken "R13", T_REG 13);
+        (stringToToken "R25", T_ERROR);
+        (stringToToken "mov", T_ERROR)]
