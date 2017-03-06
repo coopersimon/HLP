b14a765cb08f352523f20d47961c066294b00698
U-CORP\raviwoods <raviwoods@IP-C0A8DBB2.corp.amazonworkspaces.com>
2017-03-06 13:26:59 +0000

    Starting to port our project to FABLE

--- HLPProject/HLPProject/Main.fs
+++ HLPProject/HLPProject/Main.fs
@@ -8,7 +8,7 @@ let main args =
     let state = initState
     let inString = "MOV R5, #2"
     let newState = inString |> Tokeniser.tokenise |> Parser.parser |> Interpreter.interpret state
+    printfn "Start Main.fs"
     printfn "%A" (readReg state 5)
     printfn "%A" (readReg newState 5)
-
     0
