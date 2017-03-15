fd822554908928d1644db6d553cbe772a92fb6f4
U-CORP\raviwoods <raviwoods@IP-C0A8DBB2.corp.amazonworkspaces.com>
2017-03-15 14:19:03 +0000

    Full split of GetStates.fs and Main.fs

--- HLPProject/HLPProject/GetStates.fs
+++ HLPProject/HLPProject/GetStates.fs
@@ -7,7 +7,4 @@ open Common.Error
 
 module GetStates =
     let oldState = initState
-    let newState inString = 
-    match inString |> Tokeniser.tokenise |> Parser.parser |> wrapErr (Interpreter.interpret oldState) with
-    	| Ok(newState) -> printfn "%A = %A" (readReg 3 newState) (readReg 4 newState)
-    	| Err(msg) -> printfn "%s" msg
+    let newState inString =  inString |> Tokeniser.tokenise |> Parser.parser |> wrapErr (Interpreter.interpret oldState)
--- HLPProject/HLPProject/Main.fs
+++ HLPProject/HLPProject/Main.fs
@@ -2,6 +2,8 @@
 module Main
 open Execute.GetStates
 open Common.State
+open Common.Conditions
+open Common.Error
 
 [<EntryPoint>]
 let main args =
@@ -13,10 +15,12 @@ let main args =
                             ADD  R3, R3, R1
                             SUBS R2, R2, #1
                             BNE  LOOP
-                            MUL  R4, R0, R1 ; instruction mult"
+                            MUL  R4,0, R1 ; instruction mult"
     let oState = oldState
     let nState = newState inString
-    printfn "%A" (readReg 5 oState)
-    printfn "%A" (readReg 5 nState)
+    match nState with
+    | Ok(nState) -> printfn "%A = %A" (readReg 3 nState) (readReg 4 nState)
+    | Err(msg) -> printfn "%s" 
+
 
     0
