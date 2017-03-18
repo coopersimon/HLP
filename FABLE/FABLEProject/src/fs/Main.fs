77c25c5b4c4e084d52ca5daa9ab941139b3d94bc
Ravi Woods <raviwoods@MacBook-Pro.lan>
2017-03-18 21:50:41 +0000

    Styling added

--- FABLE/FABLEProject/src/fs/Main.fs
+++ FABLE/FABLEProject/src/fs/Main.fs
@@ -1,16 +1,63 @@
 module Main
-open Execute.GetStates
 open Common.State
-open Common.Conditions
-open Common.Error
+open Parse
+open Interpret
+open Fable.Core
+open Fable.Import
+open FsHtml
 
 [<EntryPoint>]
-let main args =
-    
-    let inString = "MOV R1, #5"
-    let oState = initState
-    let nState = newState oState inString
-    match nState with
-    | Ok(s) -> printfn "Valid = %A" (readReg 0 s)
-    | Err(msg) -> printfn "%s" msg
+let main args =  
+    let state = initState
+    let inString = "MOV R5, #2"
+    let newState = inString |> Tokeniser.tokenise |> Parser.parser |> Interpreter.interpret state
+    let regs = Browser.document.getElementById "regs"
+    let registerString = 
+         table [
+                "class"%="table table-striped"                
+                thead [                    
+                    tr [
+                        th %("Register")
+                        th %("Value")
+                    ]
+                ]
+                tbody [ 
+                for i in 1..15 ->
+                    tr [
+                        th %(sprintf "R%A" i)
+                        th %(sprintf "%A" (readReg newState i))
+                    ]
+
+                ]
+            ]
+    regs.innerHTML <- registerString |> Html.toString
     0
+ (*
+
+<table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+ *)
