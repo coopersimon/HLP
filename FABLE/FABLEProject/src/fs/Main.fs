9b08f7be841eb7c9e92e9598a4e569fe10f31e5b
U-CORP\raviwoods <raviwoods@IP-C0A8DBB2.corp.amazonworkspaces.com>
2017-03-18 16:24:36 +0000

    Started adding Bootstrap

--- FABLE/FABLEProject/src/fs/Main.fs
+++ FABLE/FABLEProject/src/fs/Main.fs
@@ -2,13 +2,18 @@
 open Common.State
 open Parse
 open Interpret
+open Fable.Core
+open Fable.Import
+open FsHtml
 
 [<EntryPoint>]
 let main args =  
     let state = initState
     let inString = "MOV R5, #2"
     let newState = inString |> Tokeniser.tokenise |> Parser.parser |> Interpreter.interpret state
-    printfn "Start Main.fs - 2"
-    printfn "%A" (readReg state 5)
-    printfn "%A" (readReg newState 5)
+    let regs = Browser.document.getElementById "regs"
+    let registerString = 
+        ul [for i in 1..15 ->
+            li %(sprintf "R%A = %A" i (readReg newState i)) ]
+    regs.innerHTML <- registerString |> Html.toString
     0
