58d5199a5faceb807191cc9da2fdf99d921af4b1
Piotr Pomienski <p.pomien@gmail.com>
2017-03-23 23:50:11 +0000

    Testbench updated to work with altered pipeline Added single instruction testing Code cleanup and comments

--- visual/visual/Program.fs
+++ visual/visual/Program.fs
@@ -6,6 +6,8 @@ open Interpret
 open Parse.Tokeniser
 open Common.Conditions
 open Common.Error
+open Execute.GetStates
+
 
 module Program=
 
@@ -14,9 +16,7 @@ module Program=
 
     let seqConfig = { Expecto.Tests.defaultConfig with parallel = false}
 
-    let state = initStateVisual
-
-    let getFlags (regArr, flagBool) = 
+    let getFlags (regArr, flagBool) =  // get flags into the format expected by the testbench
         let flags = ""
 
         let rec matchSeq flags flagsList =
@@ -30,26 +30,42 @@ module Program=
     let getRegs (regArr : int[], flagBool) = 
         
         let regList = List.empty
-        let rec matchRegs regFormat regNum =
+        let rec matchRegs regFormat regNum = // get all 16 registers into the list
             if (regNum < 15) then
                 let newReg =  R regNum, regArr.[regNum]
                 matchRegs (newReg :: regFormat) (regNum + 1)
             else
-                regFormat
-        matchRegs regList 0     
+                regFormat // return the correct list
+        matchRegs regList 0   
+    
+    let getMem (regArr : int[], flagBool, mem : Map<int, int>) = //gets a complete register and memory map 
+        let outList = List.empty
+        let memList = Map.toList mem
 
-    let testRegs st = (getRegs (readState st))
+        let rec matchMems regFormat mems = // for every key in the memory map, append to list
+            match mems with 
+            | (addr, value) :: tail ->  let newMem = Mem addr, value 
+                                        matchMems (newMem :: regFormat) mems.[1..]
+            | [] -> regFormat
+
+        let rec matchRegs regFormat regNum = // get all 16 registers into the list
+            if (regNum < 15) then
+                let newReg =  R regNum, regArr.[regNum]
+                matchRegs (newReg :: regFormat) (regNum + 1)
+            else
+                matchMems regFormat memList // when registers done, start parsing memory
+
+        matchRegs outList 0   
+
+    let testRegs st = (getRegs (readState st)) // functions to return registers/flags/memory content
     let testFlags st = (getFlags (readState st)) 
+    let testRegsWithMem st = (getMem (readStateWithMem st))
 
-//    let runTest instr = 
-//        match instr |> Tokeniser.tokenise |> Parser.parser |> wrapErr (Interpreter.interpret state) with
-//        | Ok(_, newState) -> ((testRegs newState), (testFlags newState))
-//        | Err(L, msg) -> failwithf "%A%s" L msg   
-
-    let runTest instr = 
-        match instr |> Tokeniser.tokenise |> Parser.parser |> wrapErr (Interpreter.interpret state) with
-        | Ok(_, newState) -> ((testRegs newState), (testFlags newState))
-        | Err(L, msg) -> failwithf "%A%s" L msg   
+    let runTest runInstr = // Handle to run emulator, runInstr containing the string with instruction{s}
+        let nState = newState runInstr
+        match nState with
+            | Ok(_,s) -> ((testRegs s), (testFlags s))
+            | Err(l,msg) -> failwithf "Line %d: %s" l msg
 
     /// postlude which sets R1 bits to status bit values
     let NZCVToR12 =
@@ -114,47 +130,44 @@ module Program=
         | [ a ; b ; c ; d] -> { FN=a; FZ=b;FC=c;FV=d}
         | _ -> failwithf "Wrong number of characters (should be 4) in flag specification %s" s
     
-    
     /// run an expecto test of VisUAL
     /// name - name of test
     ///
-    let VisualUnitTest name src  =
+    let VisualUnitTest name src  = // compare output of VisUAL with output from our program
         testCase name <| fun () ->
-            let state = initStateVisual
-            let ((outExpected: (Out * int) list), (flagsExpected:string)) = 
-                (runTest src)
+            let ((outExpected: (Out * int) list), (flagsExpected:string)) = (runTest src) // runs the instruction{s} on our emulator
             let mems = outExpected |> List.collect (function | Mem n, x -> [n,x] | _ -> [])
             let memLocs = mems |> List.map fst
-            let flags, outs = RunVisualWithFlagsOutLocs memLocs src
-            Expecto.Expect.equal flags (flagsExpected |> strToFlags) "Status flags don't match"
             let regs = outExpected |> List.filter (function | R _,_ -> true | _ -> false)
+            let flags, outs = RunVisualWithFlagsOutLocs memLocs src
             let getOut (out, v) = 
                 try
                     out, outs.[out]
                 with
                 | _ -> failwithf "Can't find output %A in outs %A" out outs
-            Expecto.Expect.sequenceEqual (outExpected |> List.map getOut) outExpected "Reg and Mem outputs don't match"
-            
-    let randomReg() (rnd : System.Random) = 
+            Expecto.Expect.sequenceEqual (outExpected |> List.map getOut) outExpected "Reg and Mem outputs don't match" //Expecto to compare outputs
+            Expecto.Expect.equal flags (flagsExpected |> strToFlags) "Status flags don't match"
+           
+    let randomReg() (rnd : System.Random) = //Generate a random register in range 0-12 (can be changed to 0-15 if for some reason PC/SP want to be written to)
         "R" + string(rnd.Next(12))
 
-    let randomImm() (rnd : System.Random) = 
+    let randomImm() (rnd : System.Random) = //Generate a random immediate - take an 8-bit integer and rotate it right by a 5-bit even integer
         let rotateRight x r = (x>>>r) ||| (x<<<(32-r))
         let getShift = (rnd.Next(15) <<< 1)
         let retval = uint32(rotateRight (rnd.Next(255)) getShift)      
         "#" + retval.ToString()
 
-    let regConstShift() (rnd : System.Random) = 
+    let regConstShift() (rnd : System.Random) = //Generate a register with constant shift - as per ARMv4 spec, probability skewed towards no shift
          match rnd.Next(9) with
          | 0 | 6 | 7 | 8 | 9 -> randomReg() rnd 
          | 1 -> randomReg() rnd + ", LSL #" + string(rnd.Next(31))
          | 2 -> randomReg() rnd + ", LSR #" + string(rnd.Next(1,32))
          | 3 -> randomReg() rnd + ", ASR #" + string(rnd.Next(1,32))
          | 4 -> randomReg() rnd + ", ROR #" + string(rnd.Next(1,31))
-         //| 5 -> randomReg() rnd + ", RRX"
+         | 5 -> randomReg() rnd + ", RRX"
          | _ -> ""
 
-    let operand2() (rnd : System.Random) = 
+    let operand2() (rnd : System.Random) = //Generate a flexible 2nd operand - as per ARMv4 spec, probability skewed towards no shift
         match rnd.Next(10) with
         | 0 | 7 | 8 | 9 | 10 -> randomImm() rnd
         | 1 -> regConstShift() rnd
@@ -165,7 +178,14 @@ module Program=
         | 6 -> randomReg() rnd + ", ROR " + randomReg() rnd
         | _ -> ""
 
-    let allInstr rnd = [
+
+    // List containing all instructions that can be run by the testbench
+    // This is not an exhaustive list of instructions supported by our emulator,
+    // but not all of our instructions are implemented in VisUAL, so not all can be model checked
+    // Those not supported by VisUAL are just unit tested by their respective owners
+    
+    // Branches omitted on purpose, adding branches to randomly generated code is a terrible idea
+    let allInstr rnd = [ 
 
         //MOV, MOVS
         ("MOV " + (randomReg() rnd) + ", " + (operand2() rnd))
@@ -174,7 +194,6 @@ module Program=
         //MVN, MVNS
         ("MVN " + (randomReg() rnd) + ", " + (operand2() rnd))
         ("MVNS " + (randomReg() rnd) + ", " + (operand2() rnd))
-
 
         //ADD, ADDS
         ("ADD " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
@@ -197,7 +216,7 @@ module Program=
         ("RSBS " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
 
         //RSC, RSCS
-        //("RSC " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
+        ("RSC " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
         ("RSCS " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
 
         //CMP, CMN
@@ -212,13 +231,13 @@ module Program=
         ("AND " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
         ("EOR " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
         ("ORR " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
-        //("ORN " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
+        ("ORN " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
         ("BIC " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
-        ("ANDS " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
-        ("EORS " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
-        ("ORRS " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
-        //("ORNS " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
-        ("BICS " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
+//        ("ANDS " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd)) // flag updates on bitwise ops don't work correctly in Visual
+//        ("EORS " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
+//        ("ORRS " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
+//        ("ORNS " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
+//        ("BICS " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (operand2() rnd))
 
         //Shifts
         ("ASR " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (randomReg() rnd))
@@ -231,50 +250,43 @@ module Program=
         ("LSRS " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (randomReg() rnd))
         ("RORS " + (randomReg() rnd) + ", " + (randomReg() rnd) + ", " + (randomReg() rnd))
         ("RRXS " + (randomReg() rnd) + ", " + (randomReg() rnd))
-
         ]
 
 
-    let rec randInstr (instrList : System.Random -> string list )(rnd : System.Random) instrStr n = 
+    let rec randInstr (instrList : System.Random -> string list )(rnd : System.Random) instrStr n = // generate a list of random instructions, containing n instructions
             match n = 0 with 
             | false ->  let instrListInst = (instrList rnd)
                         randInstr instrList rnd (instrStr + instrListInst.[rnd.Next(instrListInst.Length)] + "\n") (n-1)
             | true -> instrStr 
+
+    let oneInstr (instrList : System.Random -> string list )(rnd : System.Random) = // generate a single random instruction
+        let instrListInst = instrList rnd
+        (instrListInst.[rnd.Next(instrListInst.Length)] + "\n")
+
 
     [<EntryPoint>]
     let main argv = 
         
         let rnd = System.Random()  
             
-        let inInstr = (randInstr allInstr rnd "" 20) // generate a load of random instructions
-        printf "%A" inInstr
+        let listOfInstr = (randInstr allInstr rnd "" 10) // generate a load of random instructions
+        let singleRandInstr = (oneInstr allInstr rnd)
+        printf "\nRandomly generated instruction set: \n%A" listOfInstr
+        printf "\nSingle random instruction: \n%A" singleRandInstr
 
         InitCache defaultParas.WorkFileDir // read the currently cached info from disk to speed things up
         let visualTests = 
             testList "Visual tests" [
-                
-               // VisualUnitTest "Random Instruction Generator test" inInstr
-                    
-                VisualUnitTest "ADDS test" "EORS R0, R5, #4000
-ADCS R7, R4, #1308622848
-ORRS R4, R2, #76800
-LSR R5, R1, R3
-CMP R1, R11, ASR R4
-SUBS R6, R8, R7, ROR R0
-ADC R4, R3, R8, LSR R1
-RSCS R0, R8, R6, ASR R0
-EOR R4, R11, #3254779904
-RRX R5, R5
-LSL R2, R4, R4
-SBC R8, R9, R6, LSL R11"
-//                VisualUnitTest "SUB test" "SUB R0, R0, #1"
-//                VisualUnitTest "SUBS test" "SUBS R0, R0, #0"
-//                VisualUnitTest "SBCS test" "SUBS R0, R0, #0"
-//                VisualUnitTest "RSB test" "RSB R0, R0, #0"
-//                VisualUnitTest "RSC test" "RSB R0, R0, #0"
+             
+                VisualUnitTest "Random Instruction Generator test" listOfInstr // randomly generated set of instructions
+                VisualUnitTest "Random single instruction test" singleRandInstr // single instruction
+
+
+//                VisualUnitTest "MEM test" "MOV R0, #1 // mem tests don't work - visual interface missing
+//                                            MOV R1, #0x7F000000
+//                                            STR R0, [R1]"
             ]
         
-
         let rc = runTests seqConfig visualTests
         System.Console.ReadKey() |> ignore                
         rc // return an integer exit code - 0 if all tests pass
