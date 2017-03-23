4ca2d4378a1bcc8a2b63411a783e8f808f3fbab5
Yumeng Sun <ys7914@ic.ac.uk>
2017-03-23 20:07:44 +0000

    More changes for ARMv4 testing

--- HLPProject/ARMv4Test/ARMv4Test.fs
+++ HLPProject/ARMv4Test/ARMv4Test.fs
@@ -1,100 +1,29 @@
 ﻿//to be copied over to HLPProject Main.fs for testing
-module Main
+module ARMv4Test
+//module Main
+open Interpret.ARMv4
 open Execute.GetStates
 open Common.State
 open Common.Error
+open Common.Types
+open Common.Conditions
 
 [<EntryPoint>]
 let main args =
-    //input assembly code to be tested
-    let inStringSetZ = "MOVS R1, #0" 
-    let inStringSetN = "MOV R1, #3 
-                         SUBS R2, R1, #5" 
-    let inStringSetC = "MVN		R1, #0
-	                	ADDS	R1, R1, #7"
-    let inStringSetCV = "		MOV		R0, #1
-		                        MVN		R1, #3
-		                        MOV		R2, R0, LSL #31
-		                        ADDS	R3, R1, R2
-                                " 
-    let inStringLSL = "MOV R1, #1
-                       MOV R2, R1, LSL #3" 
-    let inStringCT2tick = "	;Par		{ R2 := 11 - R0 ; R1 := 3*R0 - 4*R1 ; R0 := R1 }
-		
-                    		MOV		R2, R0 		;R2 := R0
-                    		MOV		R0, R1 		;R0 := R1 [DONE]
-                    		ADD		R1, R1, R1 	;R1 := 2*R1
-                    		SUB		R1, R2, R1 	;R1 := R0 - 2*R1
-                    		ADD		R1, R1, R1 	;R1 := 2*R0 - 4*R1
-                    		ADD		R1, R1, R2 	;R1 := 3*R0 - 4*R1 [DONE]
-                    		RSB		R2, R2, #11 	;R2 := 11 - R0 [DONE]"
-    let inStringCT3fig3 = "	MOV		R2, #0 ; for testing
-                           		MOV		R1, #7 ; for testing
-                        		MOV		R0, #0
-                    LOOP		CMP		R2, #0
-                        		BEQ		stop
-                        		ADD		R0, R0, R1
-                        		SUB		R2, R2, #1
-                        		B		LOOP
-                    stop" //PC counts 4 lesser because of ending with label
-    let inStringCT4ticksb = "	MOV		R2, #0
-                        		AND		R4, R1, #0x80000000
-                        		AND		R5, R1, #0x40000000
-                        		CMP		R5, R4, lsr #1
-                        		MOVNE	R2, #1
-		
-                        		MOV		R0, R1, lsl #30
-                        		MOV		R3, R1, asr #2
-                        		MOV		R1, R1, lsl #1
-                        		ADDS	R1, R1, R3
-            		
-                        		MOVVS	R2, #1"
-    let inStringCT5tick = "		MOV		R0, #16
-		
-                           		ADR		R1, BLC_TABLE
-                        		CMP		R0, #15
-                        		MOVHI	R1, #0 ; error output for when input is out of range
-                        		LDRLS	R1, [R1, R0, LSL #2]
-		
-                                BLC_TABLE	DCD		85, 86, 89, 90, 101, 102, 105, 106, 149, 150, 153, 154, 165, 166, 169, 170 
-                                ;constant word table stored in memory"
-                                //DCD does not work with multiple words yet
-    let inStringLong =     "LDR     R1, =INPUT_1
-                            LDR     R2, =INPUT_2
-
-                    MAIN
-                            BL      MULT_A
-                            BL      MULT_B
-                            LDMDB   R13, {R6-R7}
-                            ADR     R10, RETURN
-                            B       FINAL
-                    RETURN  END
-
-                    MULT_A
-                            SUB     R3, R1, #1
-                    LOOP    ; Manual multiplier loop
-                            ADD     R4, R4, R2
-                            SUBS    R3, R3, #1
-                            BPL     LOOP
-                            STR     R13, [R4, #4]!
-                            MOV     PC, LR
-
-                    MULT_B
-                            MUL     R5, R1, R2 ; instruction mult
-                            STR     R13, [R5, #4]!
-                            MOV     PC, LR
-                    
-                    FINAL
-                            CMP     R6, R7
-                            MOVEQ   R0, #1
-                            MOVNE   R0, R4
-                            BX      R10
-                    INPUT_1 DCD     5
-                    INPUT_2 DCD     3"
+    //some sample input parameters
+    let c = checkAL
+    let s = false
+    let rd = 1
+    let rm = 2
+    let rn = 3
+    let rsinst = T_LSL
+    let nORrn = 5
+    let rstype = T_R
+    let i = 354
     
     //initial state
-    let oState = initState
-    match oState with
+    let state = initState
+    match state with
     | s ->          printfn " Initial values:"
                     printfn " R0 = %A" (readReg 0 s)
                     printfn " R1 = %A" (readReg 1 s)
@@ -118,10 +47,53 @@ let main args =
                     printfn " V = %A" (readVFlag s)
     | _          -> printfn "Error"
 
-    //new state after code execution
-    let nState = newState inStringCT5tick
+
+    let testfn n state = 
+        match n with
+        (*  Corresponding Assembly
+		    MOV	    	R0, #5
+		    MVNS		R1, R0, LSL #5    
+        *)
+        |1 -> writeReg 13 0xFF000000 state
+              |> movI c s 0 5 
+              |> mvnR c true 1 0 T_LSL 5 T_I 
+              |> writePC 8  
+        (*  Corresponding Assembly
+            MOV R1, #1
+            MOVS R2, R1, LSL #3  
+        *)
+        |2 -> writeReg 13 0xFF000000 state
+              |> movI c s 1 1 
+              |> movR c true 2 1 T_LSL 3 T_I 
+              |> writePC 8  
+        (*  Corresponding Assembly
+		    MOV		R1, #4 	
+            MOV		R0, #67 		
+            RSBS	R2, R0, R1, ROR R1 	  
+        *)
+        |3 -> writeReg 13 0xFF000000 state
+              |> movI c s 1 4 
+              |> movI c s 0 67
+              |> rsbR c true 2 0 1 T_ROR 1 T_R 
+              |> writePC 12  
+        (*  Corresponding Assembly
+		    MOV		R1, #77
+		    MOV		R0, #67
+		    ORRS	R2, R0, R1, RRX 	  
+        *)
+        |4 -> writeReg 13 0xFF000000 state
+              |> movI c s 1 77 
+              |> movI c s 0 67
+              |> orrR c true 2 0 1 T_RRX 1 T_R 
+              |> writePC 12  
+        |_ -> state
+
+    //function to test (can pipeline for multiple instructions)
+    let nState = testfn 4 state
+        
+    //resulting state
     match nState with
-    | Ok(_,s) ->    printfn " Resulting values:"
+    | s ->          printfn " Resulting values:"
                     printfn " R0 = %A" (readReg 0 s)
                     printfn " R1 = %A" (readReg 1 s)
                     printfn " R2 = %A" (readReg 2 s)
@@ -142,6 +114,6 @@ let main args =
                     printfn " Z = %A" (readZFlag s)
                     printfn " C = %A" (readCFlag s)
                     printfn " V = %A" (readVFlag s)
-    | Err(l,msg) -> printfn "Line %d: %s" l msg
+    | _          -> printfn "Error" 
 
     0
